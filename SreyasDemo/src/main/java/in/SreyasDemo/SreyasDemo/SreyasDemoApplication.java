c5d9a823a8e01478edf47cdb7bd69eb8b8f34a3b
shrut <shrut@DESKTOP-6GRGENH>
2020-06-09 16:19:16 +0530

    initial commit

---
+++ SreyasDemo/src/main/java/in/SreyasDemo/SreyasDemo/SreyasDemoApplication.java
@@ -0,0 +1,13 @@
+package in.SreyasDemo.SreyasDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SreyasDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SreyasDemoApplication.class, args);
+	}
+
+}
---
+++ SreyasDemo/src/test/java/in/SreyasDemo/SreyasDemo/SreyasDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package in.SreyasDemo.SreyasDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SreyasDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
